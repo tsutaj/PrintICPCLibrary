7bb7f9af21cb59d98a27355b6a5a9aa3c1ada88e
tsutaj <ybspx300@ybb.ne.jp>
2017-06-29 23:27:29 +0900

    concise core script

--- generate.sh
+++ generate.sh
@@ -5,27 +5,26 @@ if [ $# -ne 3 ]; then
   exit 1
 fi
 
-TEXFNAME=$2
 PAGETITLE="\\\\title{$3}"
 
 SCRIPTDIR=$(dirname $0)
 PATHSTR="${1}/*.cpp"
-TEXFSTR="${SCRIPTDIR}/${2}.tex"
+NAMESTR="${SCRIPTDIR}/${2}"
 
-sed "s/\\\\title{}/${PAGETITLE}/" $SCRIPTDIR/tex_head.txt >> $TEXFSTR
+sed "s/\\\\title{}/${PAGETITLE}/" $SCRIPTDIR/tex_head.txt >> $NAMESTR.tex
 
 for file in `\find $PATHSTR -maxdepth 1 -type f`; do
     FILENAME="${file##*/}"
     FILESTR=`echo "\subsection*{${FILENAME}}" | sed 's/_/\\\\_/g'`
-    echo $FILESTR >> $TEXFSTR
-    cat $SCRIPTDIR/tex_code_head.txt $file $SCRIPTDIR/tex_code_tail.txt >> $TEXFSTR
+    echo $FILESTR >> $NAMESTR.tex
+    cat $SCRIPTDIR/tex_code_head.txt $file $SCRIPTDIR/tex_code_tail.txt >> $NAMESTR.tex
 done
 
-cat $SCRIPTDIR/tex_tail.txt >> $TEXFSTR
+cat $SCRIPTDIR/tex_tail.txt >> $NAMESTR.tex
 
-platex -output-directory=$SCRIPTDIR $TEXFSTR
-dvipdfmx -o $SCRIPTDIR/$TEXFNAME.pdf $SCRIPTDIR/$TEXFNAME.dvi
+platex -output-directory=$SCRIPTDIR $NAMESTR.tex
+dvipdfmx -o $NAMESTR.pdf $NAMESTR.dvi
 
 for ext in aux dvi tex log; do
-    rm $SCRIPTDIR/$TEXFNAME.$ext
+    rm $NAMESTR.$ext
 done
